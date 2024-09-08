e8390f8bfaa7b3132b0f709fbd36ff48bbcb7d24
Alchemi1963 <alchemi1999@hotmail.com>
2024-09-08 18:29:58 +0200

    Version bump, extra info in setup.py

--- dns_mijnhost/setup.py
+++ dns_mijnhost/setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 setup(
     name='certbot-dns-mijnhost',
     package='dns_mijnhost.py',
+    version='1.1.0',
+    author="Melody Smit",
+    url="https://github.com/debolk/LetsEncrypt-mijn.host",
     install_requires=[
         'certbot',
     ],
